e18c0a30cd6be9a1de1be0b89d0274ad0ff66987
yusuf <afolesmith@gmail.com>
2020-02-13 13:27:46 +0100

    add configuration to proect

--- WebApiFundamental/App_Start/WebApiConfig.cs
+++ WebApiFundamental/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using WebApiFundamental.App_Start;
 
 namespace WebApiFundamental
 {
@@ -10,7 +11,7 @@ namespace WebApiFundamental
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            AutofacConfig.Register();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
