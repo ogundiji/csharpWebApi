0ff83fdfad44fe4943143675ce07ad9406e936b0
yusuf <ogundijiyusuf@gmail.com>
2020-02-14 06:46:30 +0100

    add migration

---
+++ WebApiFundamental/Data/CampContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApiFundamental.Data
+{
+    public class CampContext:DbContext
+    {
+        public CampContext():base("CodeCampConnectionString")
+        {   
+        }
+
+
+    }
+}
---
+++ WebApiFundamental/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace WebApiFundamental.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApiFundamental.Data.CampContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(WebApiFundamental.Data.CampContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
