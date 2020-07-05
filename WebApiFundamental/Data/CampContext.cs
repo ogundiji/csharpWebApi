4afca8ab1d6e9e75d671a8c4cf6fdc97e4b3d22f
yusuf <ogundijiyusuf@gmail.com>
2020-07-05 18:39:48 +0100

    add identity and token authentication

--- WebApiFundamental/Data/CampContext.cs
+++ WebApiFundamental/Data/CampContext.cs
@@ -1,13 +1,19 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using WebApiFundamental.Data.Entities;
 
 namespace WebApiFundamental.Data
 {
-    public class CampContext:DbContext
+    public class CampContext:IdentityDbContext<ApplicationUser>
     {
         public CampContext():base("CodeCampConnectionString")
         {   
 
+        }
+
+        public static CampContext Create()
+        {
+            return new CampContext();
         }
 
         public DbSet<Camp> camps { get; set; }
---
+++ WebApiFundamental/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using WebApiFundamental.Data;
+using WebApiFundamental.Data.provider;
+
+[assembly: OwinStartup(typeof(WebApiFundamental.Startup))]
+
+namespace WebApiFundamental
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            // Configure the db context and user manager to use a single instance per request
+            app.CreatePerOwinContext(CampContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+
+            // Plugin the OAuth bearer JSON Web Token tokens generation and Consumption will be here
+
+        }
+    }
+}
